a5223f6ab10d3392d4580f7c14a8c831eeca46f1
Guillaume Le Louarn <7guillaumelelouarn@gmail.com>
2021-02-22 22:22:36 +0100

    add season-list component

--- src/app/season/season.module.ts
+++ src/app/season/season.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SeasonComponent } from './season.component';
+import { SeasonListComponent } from './pages/season-list/season-list.component';
 
 
 
 @NgModule({
-  declarations: [SeasonComponent],
+  declarations: [SeasonComponent, SeasonListComponent],
   imports: [
     CommonModule
   ]
